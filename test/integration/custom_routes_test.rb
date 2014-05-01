e51e7256e54547ddc85b4f8cae59c8a2adf9a747
Behfar Bastani <behfar.bastani@gmail.com>
2014-04-30 20:08:49 -0700

    Added integration tests for the custom routes.

---
+++ test/integration/custom_routes_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+	test "that /login route results in :success" do
+		get '/login'
+		assert_response :success
+	end
+
+	test "that /logout route results in :redirect" do
+		delete '/logout'
+		assert_response :redirect
+		assert_redirected_to '/'
+	end
+
+	test "that /register route results in :success" do
+		get '/register'
+		assert_response :success
+	end
+end
